8dfeaa13751f7a47d7d6bb3a426399d36732e40f
Krzysztof Tomicki <tomicki.krzysztof.pv@gmail.com>
2025-11-24 18:18:10 +0100

    [Firmware] Setup UART communication channels

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,7 +1,30 @@
 #include <DFRobot_SIM7070G.h>
 
+#define PIN_TX 7
+#define PIN_RX 8
+
+SoftwareSerial shieldSerial(PIN_RX, PIN_TX);
+DFRobot_SIM7070G SIM7070G(&shieldSerial);
+
 void setup() 
 {
+    delay(10000);
+
+    // hardware UART communication with serial monitor
+    Serial.begin(115200);
+
+    // software UART communication between Arduino and SIM7070G shield 
+    shieldSerial.begin(19200);
+    
+    Serial.println("Turning ON SIM7070G...");
+    if (SIM7070G.turnON())
+    {
+        Serial.println("SIM7070G turned ON");
+    }
+    else
+    {
+        Serial.println("Connot turn ON SIM7070G");
+    }
 
 }
 
