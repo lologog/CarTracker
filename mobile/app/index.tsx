9ee5e6efcc0f75421d2e8eb3c3946682a6b497a2
Krzysztof Tomicki <tomicki.krzysztof.pv@gmail.com>
2026-04-29 16:16:29 +0200

    [MOBILE] Add Pokemon images

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
-import { ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 
 interface Pokemon {
   name: string;
-  url: string;
+  image: string;
+  imageBack: string;
 }
 
 export default function Index() {
@@ -18,7 +19,20 @@ export default function Index() {
       const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=20");
       const data = await response.json();
 
-      setPokemons(data.results);
+      // Fetch detailed info for each Pokemon in parallel
+      const detailedPokemons = await Promise.all(
+        data.results.map(async (pokemon: any) => {
+          const res = await fetch(pokemon.url);
+          const details = await res.json();
+          return {
+            name: pokemon.name,
+            image: details.sprites.front_default, // Main sprite
+            imageBack: details.sprites.back_default,
+          };
+        })
+      );
+
+      setPokemons(detailedPokemons);
     } catch (e) {
       console.log(e);
     }
@@ -29,6 +43,10 @@ export default function Index() {
       {pokemons.map((pokemon) => (
         <View key={pokemon.name}>
           <Text>{pokemon.name}</Text>
+          <View style={{flexDirection: "row"}}>
+            <Image source={{uri: pokemon.image}} style={{ width: 150, height: 150}}/>
+            <Image source={{uri: pokemon.imageBack}} style={{ width: 150, height: 150}}/>
+          </View>
         </View>
       ))}
     </ScrollView>
