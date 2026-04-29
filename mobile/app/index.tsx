6c3ee81b4710796423f486ca691e5e5f92396fb6
Krzysztof Tomicki <tomicki.krzysztof.pv@gmail.com>
2026-04-29 17:00:18 +0200

    [MOBILE] Add background tiles

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,45 @@
 import { useEffect, useState } from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 
 interface Pokemon {
   name: string;
   image: string;
   imageBack: string;
+  types: PokemonType[];
 }
+
+interface PokemonType {
+  type: {
+    name: string;
+    url: string;
+  }
+}
+
+const colorsByType: Record<string, string> = {
+  normal: "#A8A77A",
+  fire: "#EE8130",
+  water: "#6390F0",
+  electric: "#F7D02C",
+  grass: "#7AC74C",
+  ice: "#96D9D6",
+  fighting: "#C22E28",
+  poison: "#A33EA1",
+  ground: "#E2BF65",
+  flying: "#A98FF3",
+  psychic: "#F95587",
+  bug: "#A6B91A",
+  rock: "#B6A136",
+  ghost: "#735797",
+  dragon: "#6F35FC",
+  dark: "#705746",
+  steel: "#B7B7CE",
+  fairy: "#D685AD",
+};
 
 export default function Index() {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
+  console.log(JSON.stringify(pokemons[0], null, 2));
   useEffect(() => {
     fetchPokemons();
   }, []);
@@ -28,6 +58,7 @@ export default function Index() {
             name: pokemon.name,
             image: details.sprites.front_default, // Main sprite
             imageBack: details.sprites.back_default,
+            types: details.types,
           };
         })
       );
@@ -39,10 +70,11 @@ export default function Index() {
   }
 
   return (
-    <ScrollView>
+    <ScrollView contentContainerStyle={{gap: 16, padding: 16}}>
       {pokemons.map((pokemon) => (
-        <View key={pokemon.name}>
-          <Text>{pokemon.name}</Text>
+        <View key={pokemon.name} style={{backgroundColor: colorsByType[pokemon.types[0].type.name] + 50, padding: 20, borderRadius: 20}}>
+          <Text style={styles.name}>{pokemon.name}</Text>
+          <Text style={styles.type}>{pokemon.types[0].type.name}</Text>
           <View style={{flexDirection: "row"}}>
             <Image source={{uri: pokemon.image}} style={{ width: 150, height: 150}}/>
             <Image source={{uri: pokemon.imageBack}} style={{ width: 150, height: 150}}/>
@@ -52,3 +84,17 @@ export default function Index() {
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  name: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: "center"
+  },
+  type: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'grey',
+    textAlign: "center"
+  }
+})
